c7ffdcf1525866be661e454e12bd531d4e8b9f25
Ivan <peter_ivan_florendo@dlsu.edu.ph>
2025-10-27 15:03:50 +0800

    Testing the commit

---
+++ DBApplication/src/Main.java
@@ -0,0 +1,74 @@
+ import java.sql.Connection;
+ import java.sql.DriverManager;
+ import java.sql.PreparedStatement;
+ import java.sql.ResultSet;
+ import java.sql.SQLException;
+//import java.sql.*;
+
+public class Main {
+
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/dbworld";
+    private static final String USER = "root";
+    private static final String PASSWORD = "yumeKaze17";
+
+    private static Connection conn = null;
+
+    //1. Establish a connection
+    public static void connectDB()
+    {
+        try {
+            //driver manager - 
+            conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            System.out.println("Connected to database!");
+        } catch (SQLException ex) {
+            System.getLogger(Main.class.getName()).log(System.Logger.Level.ERROR, (String) null, ex);
+        }
+    }
+
+    public static void getCountryLanguage(String countryCode, String official){
+        StringBuilder query = new StringBuilder();
+        query.append (" SELECT *               ");
+        query.append (" FROM   Country Language");
+        query.append (" FROM   CountryCode = ? ");
+
+        if(official.equals("T") || official.equals("F"))
+        {
+            query.append(" AND IsOfficial = ?");
+        }
+
+        try{
+            PreparedStatement = stmt; //PreparedStatement
+            stmt = conn.prepareCall(query.toString());
+            query.toString(1, countryCode);
+
+            if(official.equals("T") || official.equals("F"))
+            {
+                stmt.setString(2, official);
+            }
+
+            ResultSet rs; //this returns the result of the query
+            rs = stmt.executeQuery(); //returns true if there is at least 1 record in the query
+
+            while(rs.next())
+            {
+                String code = rs.getString("CountryCode");
+                String language = rs.getString("Language");
+                String isOfficial = rs.getString ("IsOfficial");
+                double percentage = rs.getDouble("Percentage");
+                System.out.println(code + ", " + language + ", " + isOfficial + ", " + percentage);
+            }
+
+            rs.close();
+            stmt.close();
+            conn.close();
+        }
+    }
+
+    public static void main(String[] args) {
+      System.out.println("Testing DB Connection...");
+      connectDB();
+      getCountryLanguage("PHL", "F");
+
+    }
+        
+}
